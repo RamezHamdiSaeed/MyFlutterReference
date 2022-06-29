3630a78ce5af71542efb591a3fbb422c7f00496e
ramez1212 <317170274@sha.edu.eg>
2022-06-29 16:22:37 +0200

    keep digging into flutter Widgets

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## What is achieved so far ?
+- creating flutter app from scratch
+- understanding the basics of flutter
+- organize the code in a way that is easy to read and maintain
+- keep digging into the flutter framework especially the widgets
--- lib/App/my_app.dart
+++ lib/App/my_app.dart
@@ -6,21 +6,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.add_alert),
-        title: const Text('first app from scratch '),
-        // actions: const <Widget>[
-        //   Icon(Icons.add),
-        // ],
-      ),
-      body: Container(
-          alignment: Alignment.center,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[Text('Hello World')],
-          )),
-    ));
+          appBar: AppBar(
+            leading: const Icon(Icons.add_alert),
+            title: const Text('first app from scratch '),
+            // actions: const <Widget>[
+            //   Icon(Icons.add),
+            // ],
+          ),
+          body: Container(
+              padding: const EdgeInsets.only(top: 200),
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[Text('Hello World')],
+              )),
+        ));
   }
 }
