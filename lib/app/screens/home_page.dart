a187b521045844cfba269e6fca54912f590f166d
ramez1212 <317170274@sha.edu.eg>
2022-10-22 16:16:19 +0200

    dealing with appbars with custom features

--- lib/app/screens/home_page.dart
+++ lib/app/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 
@@ -5,26 +7,37 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 //* adding leading Icon
 //* adding trailing Icons by passing them to actions
-
+//! using customized app bar
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.account_balance_sharp),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.account_balance_wallet),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.remove_circle_rounded),
+      body: Center(
+          child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.black, Colors.white, Colors.black])),
+            ),
+            leading: const Icon(Icons.account_balance_sharp),
+            actions: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.account_balance_wallet),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.remove_circle_rounded),
+              )
+            ],
+            title: const Text("MoreUI", style: TextStyle(color: Colors.black)),
+            centerTitle: true,
           )
         ],
-        title: const Text("MoreUI"),
-        centerTitle: true,
-      ),
+      )),
       floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
           //! you need to pass the context
           onPressed: () {
             showToast("Your Empty App Is Hacked :-) ",
